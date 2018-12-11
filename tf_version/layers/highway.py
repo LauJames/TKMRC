27a7aad7c3ccafcfff8f03b38e597022a0650b1c
LauJames <LauJames_work@163.com>
2018-12-11 14:26:38 +0800

    + add highway network layer

---
+++ tf_version/layers/highway.py
@@ -0,0 +1,23 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : TKMRC 
+@File     : highway.py
+@Time     : 2018/12/9 17:36
+@Software : PyCharm
+@Copyright: "Copyright (c) 2018 Lau James. All Rights Reserved"
+"""
+
+def highway_layer(x, size, activation, carry_bias=-1.0):
+    """
+
+    :param x:
+    :param size:
+    :param activation:
+    :param carry_bias:
+    :return:
+    """
